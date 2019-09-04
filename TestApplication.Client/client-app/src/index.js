1e62e17049117408f484e78ac951b7b469b9d5db
F.Celestin <F.Celestin@8NY5ZW2>
2019-09-04 15:55:20 +0100

    added redux

--- TestApplication.Client/client-app/src/index.js
+++ TestApplication.Client/client-app/src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import axios from 'axios';
 
+const rootReducer = combineReducers({
+    tr: testReducer,
+})
+
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+
 axios.defaults.baseURL = 'https://localhost:44384';
 
 const app = (
+    <Provider store={store}>
         <BrowserRouter>
             <App></App>
         </BrowserRouter>
+        </Provider>
 );
 
 
