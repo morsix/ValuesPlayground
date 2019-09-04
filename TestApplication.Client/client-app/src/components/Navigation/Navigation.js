335221e8246bc0723c47c8ec9ef0842b9729b46e
F.Celestin <F.Celestin@8NY5ZW2>
2019-09-04 13:23:39 +0100

    navbar collapsable working

--- TestApplication.Client/client-app/src/components/Navigation/Navigation.js
+++ TestApplication.Client/client-app/src/components/Navigation/Navigation.js
@@ -1,32 +1,45 @@
-import React from 'react';
-import  "./Navigation.css";
+import React, {useState} from 'react';
+import "./Navigation.css";
 import { NavLink } from "react-router-dom";
 import Logo from './../Logo/Logo';
 
-const Navigation = props =>{
+const Navigation = props => {
+    const [showNav, setShowNav] = useState(false);
 
-    const navbar =  <nav className="tb-navbar navbar navbar-expand-md navbar-dark bg-dark">
-    <NavLink className="tb-logo navbar-brand" to="/" exact ><Logo></Logo></NavLink>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
+    const toggleNav = () => {
+      setShowNav(!showNav);
+    }
 
-    <div className="collapse navbar-collapse" id="navbarsExample04">
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/cars" exact >Cars <span className="sr-only">(current)</span></NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to='/help' exact >Help</NavLink>
-        </li>
-      </ul>
-      <form className="form-inline my-2 my-md-0">
-        <input className="form-control" type="text" placeholder="Search"></input>
-      </form>
-    </div>
-  </nav>
+    let navStyles;
 
-        return <header>{navbar}</header>;
+    if(showNav){
+      navStyles = ['collapse', 'navbar-collapse','show']
+    }else{
+      navStyles = ['collapse', 'navbar-collapse']
+    }
+
+    const navbar = <nav className="tb-navbar navbar navbar-expand-md navbar-dark bg-dark">
+        <NavLink className="tb-logo navbar-brand" to="/" exact ><Logo></Logo></NavLink>
+        <button className="navbar-toggler" type="button" onClick={toggleNav}>
+            <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className={navStyles.join(' ')} >
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/cars" exact >Cars <span className="sr-only">(current)</span></NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to='/help' exact >Help</NavLink>
+                </li>
+            </ul>
+            <form className="form-inline my-2 my-md-0">
+                <input className="form-control" type="text" placeholder="Search"></input>
+            </form>
+        </div>
+    </nav>
+
+    return <header>{navbar}</header>;
 }
 
 export default Navigation;
--- TestApplication.Client/client-app/src/index.js
+++ TestApplication.Client/client-app/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
